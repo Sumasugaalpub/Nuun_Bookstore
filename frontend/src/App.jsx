44078834c7387b6eae303ecc07cf2284d7abe168
Sumayo Sugaal <butterflyscoopsugaal@gmail.com>
2024-11-19 12:21:19 +0300

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+
+import { useContext } from 'react'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import { AuthContext } from './contexts/AuthProvider'
+import Newnaav from './pages/Home/Newnaav'
+// import Navbar from './pages/shared/Navbar'
+import FooterMain from './pages/shared/FooterMain'
+
+function App() {
+
+  const {user} = useContext(AuthContext)
+  return (
+    <>
+          <Newnaav/>
+      {/* <Navbar/> */}
+      <div className='min-h-screen'>
+      <Outlet/>
+      </div>
+      <FooterMain/>
+    </>
+  )
+}
+
+export default App
